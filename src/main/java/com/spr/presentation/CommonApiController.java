8dfb5ab80d7d4ed02a44896de71c01c663acc5f3
ririso <ririsowdev@gmail.com>
2025-07-28 15:18:09 +0900

    不要なインポート文を削除

--- src/main/java/com/spr/presentation/CommonApiController.java
+++ src/main/java/com/spr/presentation/CommonApiController.java
@@ -3,12 +3,8 @@ package com.spr.presentation;
 
 import com.spr.application.usecase.GetTaskUseCase;
 import com.spr.application.usecase.GetTasksUseCase;
-import com.spr.application.usecase.GetGoodUseCase;
-import com.spr.application.usecase.GetGoodsUseCase;
-import com.spr.generated.model.GetCommonGoodsResponse;
 import com.spr.generated.model.GetCommonTasksResponse;
 import com.spr.generated.model.Task;
-import com.spr.generated.model.Good;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +27,7 @@ public class CommonApiController implements CommonApi {
     public ResponseEntity<GetCommonTasksResponse> getTasks(Integer userId) {
         final var tasksList = getTasksUseCase.execute(userId);
 
-//        Mapperで取得したTaskをOpenApiで定義したResponseのTask型に変換する
+        // Mapperで取得したTaskをOpenApiで定義したResponseのTask型に変換する
         final var taskResponseList = tasksList.stream()
                 .map(task -> new Task(task.taskId(),task.userId(), task.taskName())) // task.id(), task.name() でデータを取り出し
                 .toList();
